fe77e212e8e2a1f670560bf7aadd2682b6bc5726
Uli__ <ulisescolin332@gmail.com>
2025-11-14 23:05:02 -0600

    Create Ejercicio1a.c

---
+++ Ejercicio1a.c
@@ -0,0 +1,33 @@
+/*Francisco Colín Ulises
+Práctica 11, Programa 1a*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Implementación de las funciones del programa
+void imprimir(char s[])
+{
+  int tam;
+  for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+  printf("%c", s[tam]);
+  printf("\n");
+}
+
+void imprimir2(char s[])
+{
+    int tam, i;
+    tam = strlen(s) - 1;
+    
+    for (i = 0;  i <= tam; i++)
+    printf("%c", s[i]);
+    printf("\n");
+}
+
+// Definición o implementación de la función main
+int main()
+{
+    char nombre[] = "Facultad de Ingenieria";
+    imprimir(nombre);
+    imprimir2(nombre);
+    return 0;
+}
